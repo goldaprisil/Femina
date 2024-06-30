1bf403032638cd8787cf29bb532f6fad5bf03fde
Golda Priscilia <golda.prsclia@gmail.com>
2024-07-01 00:23:23 +0700

    Update style

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
-// 6. Function hendleGetFormData
-function hendleGetFormData(){
+// 6. Function handleGetFormData
+function handleGetFormData() {
     let name = document.getElementById("name").value;
     let city = document.getElementById("city").value;
     let email = document.getElementById("email").value;
     let zipcode = document.getElementById("zip-code").value;
-    let status = document.getElementById("check").checked;
+    let status = document.getElementById("status").checked;
     return {name, city, email, zipcode, status};
 }
 
 // 7. Function isNumber
-function isNumber(str){
+function isNumber(str) {
     return !isNaN(str);
 }
 
 // 8. Function checkboxIsChecked
-function checkboxIsChecked(){
-    let checkboxStatus = document.getElementById("check");
+function checkboxIsChecked() {
+    let checkboxStatus = document.getElementById("status");
     return checkboxStatus.checked;
 }
 
 // 9. Function validateFormData
-function validateFormData(obj){
+function validateFormData(obj) {
     return obj !== null && isNumber(obj.zipcode) && checkboxIsChecked();
 }
 
 // 10. Function Submit
-function submit(event){
-    let formData = hendleGetFormData();
-    let isValid = validateFormData(formData)
+function submit(event) {
+    event.preventDefault();
+    let formData = handleGetFormData();
+    let isValid = validateFormData(formData);
     let warning = document.getElementById('warning');
-    if(!isValid){
-        warning.textContent = 'Periksa form anda sekali lagi'
+    if (!isValid) {
+        warning.textContent = 'Periksa form anda sekali lagi';
+    } else {
+        warning.textContent = 'Form berhasil dikirim!';
     }
 }
-document.getElementById('submit-form').addEventListener("click", (event) => {
-    event.preventDefault();
-    submit()
-});
+
+document.getElementById('submit-form').addEventListener("click", submit);
